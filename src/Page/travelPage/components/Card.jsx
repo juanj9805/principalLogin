b21e858bff3ee203e9698d6a23a10db567cc4f80
Juanj9805 <Juanjoseb9805@gmail.com>
2024-02-11 05:53:25 -0500

    primera migracion

--- src/Page/travelPage/components/Card.jsx
+++ src/Page/travelPage/components/Card.jsx
@@ -1,5 +1,6 @@
 import { Button } from 'antd'
 import React, { useEffect } from 'react'
+import exampleImage from '../../../assets/cartagena.svg';
 
 export default function Card({ejemplo, data}) {
     const handleButtonClick = () => {
@@ -19,35 +20,51 @@ export default function Card({ejemplo, data}) {
     style={{
         width:"400px",
         height:"500px",
-        border:"solid red 3px"
-    }}>Card
-                <div style={{
-                    border:"solid blue 3px "
+        border:"solid red 3px",
+        borderRadius:"10px",
+        background:"#fff",
+        display:"flex",
+        flexDirection:"column",
+        justifyContent:"space-evenly"
+
+    }}>
+                <img src={exampleImage} style={{
+                    border:"solid blue 3px ",
+                    margin:"10px",
+                    borderRadius:"10px"
                 }}>
-                    {ejemplo}
+              
+                </img>
+
+                <div style={{
+                    border:"solid red 3px ",
+                    margin:"0px 10px"
+                }}>
+                          <h2>Cartagena</h2>
+                    {/* {ejemplo} */}
                 </div>
 
                 <div style={{
-                    border:"solid red 3px "
+                    border:"solid yellow 3px ",
+                    margin:"5px 10px"
                 }}>
-                    rojo:{idPaquete}
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quod at animi id eligendi ad necessitatibus debitis odio deserunt incidunt, quibusdam enim impedit quisquam obcaecati, exercitationem ea voluptate, consequatur maiores?
                 </div>
 
-                <div style={{
-                    border:"solid yellow 3px "
-                }}>
-                     Amillo: { data.descripcionPaquete}
-                </div>
-
-                <div style={{
+                {/* <div style={{
                     border:"solid green 3px "
                 }}>
                     verde
-                </div>
+                </div> */}
+
+                <Button 
+                onClick={handleButtonClick}>
+                     Ver
+                </Button>
 
                 <Button onClick={handleButtonClick}>
-        prueba
-      </Button>
+                     Comprar
+                </Button>
 
                
                 
